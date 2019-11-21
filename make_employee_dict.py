6ffd811a698a6665851490498d5346f9690c2aaf
brianhoang <hoangbr@oregonstate.edu>
2019-11-20 23:07:50 -0800

    8c

--- make_employee_dict.py
+++ make_employee_dict.py
@@ -6,10 +6,10 @@ class Employee:
         self.email_address = email_address
 
     dic_employee = {}
-    def make_employee_dict(self, name, ID_number, salary, email_address):
-        tally = 0
-        for tally in range(len(self.name)):
-            self.name[tally] = Employee(self.ID_number[tally], self.salary[tally], self.email_address[tally])
-
-            dic_employee[self.ID_number[tally]] = self.name[tally]
-        return dic_employee
+def make_employee_dict(self, name, ID_number, salary, email_address):
+    tally = 0
+    for tally in range(len(self.name)):
+        self.name[tally] = Employee(self.ID_number[tally], self.salary[tally], self.email_address[tally])
+        dic_employee[self.ID_number[tally]] = self.name[tally]
+        tally+=1
+    return dic_employee
